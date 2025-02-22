edd03d4190903d05166a04b1541ee78c292b9ac3
andresmotta9 <46831406+andresmotta9@users.noreply.github.com>
2025-02-22 16:32:27 -0500

    Update tests and linter

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// eslint.config.js
+module.exports = [
+  {
+    files: ['*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        console: 'readonly',
+        // Node globals
+        require: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+      },
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+  },
+];
